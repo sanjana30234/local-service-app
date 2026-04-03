f00252b7e7734be2d6be21e8fda870181471432a
Sanjana <sanjanaganathe467@gmail.com>
2026-04-03 11:58:23 +0530

    fixed file name mismatch

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Login from "./pages/login";
-import Services from "./pages/services";
-import Bookings from "./pages/bookings";
+import Service from "./pages/Service";
+import Bookings from "./pages/Bookings";
+import Home from "./pages/home";
 
 function Navbar() {
   return (
@@ -32,28 +33,6 @@ const linkStyle = {
   textDecoration: "none"
 };
 
-function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "80px" }}>
-      <h1>Welcome to Local Services 🔧</h1>
-      <p>Book electricians, cleaners and more!</p>
-
-      <Link to="/services">
-        <button style={btnBlue}>Explore Services</button>
-      </Link>
-    </div>
-  );
-}
-
-const btnBlue = {
-  padding: "12px 25px",
-  background: "#007bff",
-  color: "white",
-  border: "none",
-  borderRadius: "5px",
-  marginTop: "20px"
-};
-
 function App() {
   return (
     <Router>
@@ -61,7 +40,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
+        <Route path="/services" element={<Service />} />
         <Route path="/bookings" element={<Bookings />} />
         <Route path="/login" element={<Login />} />
       </Routes>
